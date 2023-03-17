5da9f23f3d52f631b0c11b948d86a5f5183c4629
aqude <aqu.de@yandex.ru>
2023-03-18 01:03:51 +0300

    add search elements

--- app/src/main/java/com/example/browserkpk/screens/SearchScreen.kt
+++ app/src/main/java/com/example/browserkpk/screens/SearchScreen.kt
@@ -3,15 +3,23 @@ package com.example.browserkpk.screens
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.runtime.*
 
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -23,6 +31,9 @@ import com.example.browserkpk.controller.Screen
 
 @Composable
 fun WallpaperSearch(navController: NavController) {
+    var searchLink by remember {
+        mutableStateOf("")
+    }
     val background = painterResource(R.drawable.image_3)
     Box(
         modifier = Modifier.fillMaxSize()
@@ -35,28 +46,70 @@ fun WallpaperSearch(navController: NavController) {
             modifier = Modifier.fillMaxSize()
         )
     }
-    Column(horizontalAlignment = Alignment.End, modifier = Modifier.fillMaxSize()) {
-        IconButton(
-            modifier = Modifier
-                .padding(end = 16.dp, top = 16.dp),
-            onClick = {navController.navigate(Screen.Setting.route)}
-        ) {
-            Icon(
-                Icons.Filled.Settings,
-                contentDescription = "Settings",
+    Column(modifier = Modifier
+        .fillMaxSize())
+    {
+        Column(horizontalAlignment = Alignment.End, modifier = Modifier.fillMaxWidth())
+        {
+            IconButton(
                 modifier = Modifier
-                    .size(50.dp)
-                    .background(Color.Transparent),
-                tint = Color.White
-            )
+                    .padding(end = 16.dp, top = 16.dp),
+                onClick = { navController.navigate(route = Screen.Setting.route) }
+            ) {
+                Icon(
+                    Icons.Filled.Settings,
+                    contentDescription = "Settings",
+                    modifier = Modifier
+                        .size(50.dp)
+                        .background(Color.Transparent),
+                    tint = Color.White
+                )
+            }
         }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(20.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = CenterHorizontally
+        )
+        {
+            Row(verticalAlignment = CenterVertically, modifier = Modifier.padding(horizontal = 40.dp)) {
+                TextField(
+                    modifier = Modifier
+                        .weight(1f)
+                        .background(Color.White, RoundedCornerShape(20.dp))
+                        .clip(RoundedCornerShape(20.dp))
+                        ,
+                    value = searchLink,
+                    onValueChange = { searchLink = it }
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                IconButton(
+                    onClick = {},
+                    modifier = Modifier
+                        .background(Color.White, RoundedCornerShape(20.dp))
+                        .align(Alignment.CenterVertically)
+                ) {
+                    Icon(
+                        tint = Color.Black,
+                        imageVector = Icons.Default.Search,
+                        contentDescription = "search in internet"
+                    )
+                }
+            }
+
+        }
+
+
     }
+
 }
 
 @Composable
 @Preview(showBackground = true)
 fun WallpaperSearchPreview() {
 //    val background = painterResource(R.drawable.image_3)
-    WallpaperSearch(navController = rememberNavController( ))
+    WallpaperSearch(navController = rememberNavController())
 }
 
